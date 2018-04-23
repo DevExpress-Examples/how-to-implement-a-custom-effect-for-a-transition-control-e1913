39d230cddb6b9a6e397f19692d8f0e29ad13a346
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Implement a Custom Effect for a Transition Control
+
+
+<p>The following example shows how to implement a custom transition effect for a TransitionControl.</p>
+
+<br/>
+
+
---
+++ VB/AgTransitionControl_CustomEffect/MainPage.xaml.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+
+Namespace AgTransitionControl_CustomEffect
+	Partial Public Class MainPage
+		Inherits UserControl
+		Private index As Integer
+		Public Sub New()
+			InitializeComponent()
+			trControl.Content = GetElement("r0")
+		End Sub
+		Private Sub trControl_MouseLeftButtonUp(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
+			If index = 2 Then
+				index = 0
+			Else
+				index += 1
+			End If
+			trControl.Content = GetElement("r" & index.ToString())
+		End Sub
+		Private Function GetElement(ByVal id As String) As FrameworkElement
+			Return CType((CType(LayoutRoot.Resources(id), DataTemplate)).LoadContent(), FrameworkElement)
+		End Function
+	End Class
+End Namespace
