88fa8000dfed3d082d862a628fecbc0a3239d9a2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgTransitionControl_CustomEffect/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace AgTransitionControl_CustomEffect {
+    public partial class MainPage : UserControl {
+        int index;
+        public MainPage() {
+            InitializeComponent();
+            index = 0;
+            trControl.Content = GetElement("r" + index.ToString());
+        }
+        private void trControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            if (index == 2)
+                index = 0;
+            else
+                index++;
+            trControl.Content = GetElement("r" + index.ToString());
+        }
+        private FrameworkElement GetElement(string id) {
+            return (FrameworkElement)((DataTemplate)LayoutRoot.Resources[id]).LoadContent();
+        }
+    }
+}
---
+++ VB/AgTransitionControl_CustomEffect/MainPage.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Input
+
+Namespace AgTransitionControl_CustomEffect
+	Partial Public Class MainPage
+		Inherits UserControl
+		Private index As Integer
+		Public Sub New()
+			InitializeComponent()
+			index = 0
+			trControl.Content = GetElement("r" & index.ToString())
+		End Sub
+		Private Sub trControl_MouseLeftButtonUp(ByVal sender As Object, _
+				ByVal e As MouseButtonEventArgs)
+			If index = 2 Then
+				index = 0
+			Else
+				index += 1
+			End If
+			trControl.Content = GetElement("r" & index.ToString())
+		End Sub
+		Private Function GetElement(ByVal id As String) As FrameworkElement
+			Return CType((CType(LayoutRoot.Resources(id), DataTemplate) _
+				).LoadContent(), FrameworkElement)
+		End Function
+	End Class
+End Namespace
